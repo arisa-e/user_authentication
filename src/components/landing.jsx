cb8f811820985faa6e6c527d6d03a18deb713a33
arisa-e <emmanuelarisa@outlook.com>
2022-03-07 23:19:57 +0300

    Added logout

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useState } from "react"
+import { Button, Card, Alert } from 'react-bootstrap'
+import { useAuth } from "./AuthContext"
+import { Link, useNavigate } from "react-router-dom"
 
 const Landing = () => {
+
+  const [error, setError] = useState("")
+  const { currentUser, logout }=useAuth()
+  const navigate = useNavigate()
+
+  async function handleLogout(){
+    try{
+      setError("")
+      await logout()
+      navigate("/signin")
+
+    }catch(err){
+      err(err.message)
+    }
+  }
+
   return (
-    <div>Landing</div>
+    <>
+      <Card>
+      {error && <Alert variant="danger">{error}</Alert>}
+      <h2 className="text-center mb-4">Profile</h2>
+      <strong>Email:</strong>{currentUser.email}
+      <Link to ="/update" className="btn btn-primary w-100 mt-3">Update</Link>
+        
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link"onClick={handleLogout}>Logout</Button>  
+      </div>
+    </>
   )
 }
 
