7ad867fec4443d44f715bd66e618a94d05feb16b
arisa-e <emmanuelarisa@outlook.com>
2022-03-08 20:07:56 +0300

    Update privateRoute.js

--- src/privateRoute.js
+++ src/privateRoute.js
@@ -1,15 +1,7 @@
-import { Route, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "./components/AuthContext"
 
-export default function PrivateRoute({ component: Component, ...rest }) {
-    const { currentUser } = useAuth()
-  
-    return (
-      <Route
-        {...rest}
-        render={props => {
-          return currentUser ? <Component {...props} /> : <Navigate to="/login" />
-        }}
-      ></Route>
-    )
-  }
+export default function PrivateRoute({ children }) {
+  const {currentUser} = useAuth();
+  return currentUser ? children : <Navigate to="/" />;
+}
